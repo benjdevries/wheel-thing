18ccdb7e1f542b46230b1e38e1d0e717f13faacc
Ben DeVries <me@ben-devries.com>
2023-03-28 23:06:22 -0400

    start canvas

--- src/components/wheel/index.tsx
+++ src/components/wheel/index.tsx
@@ -1,7 +1,29 @@
-import react from "react"
+import React from "react";
+import "./index.css";
 
-type Props = {people: {id: number, name: string}[]}
+type Props = {
+  people: {
+    id: number;
+    name: string;
+  }[];
+};
 
 export const WheelSpinner: React.FC<Props> = (props) => {
-    return <>Hello World</>
-}
+  const canvasRef = React.useRef<HTMLCanvasElement | null>(null);
+
+  React.useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      const ctx = canvas.getContext("2d")!;
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      ctx.fillStyle = "#000000";
+      ctx.beginPath();
+      ctx.arc(50, 100, 10, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+    
+  });
+
+  return <canvas ref={canvasRef} />;
+};
